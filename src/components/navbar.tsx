97456efad7ccea40b0b66b1c2ee18fe52bb12273
eunyeong1113 <eylee1113@naver.com>
2023-12-15 22:20:14 +0900

    [#1]Feat : 모바일 화면 시 네비게이션 바 토글기능 추가

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,78 +3,69 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import {css} from '@emotion/react';
+import styled from "@emotion/styled/macro";
+import { FaBars } from "react-icons/fa";
  
-const Nav = () => {
-    // const [isHovering,setIsHovering] = useState(false);
-    //  const handleMouseOver = () => {
-    //     setIsHovering(true);
-    // };    
 
-    // const handleMouseOut = () => {
-    //     setIsHovering(false);
-    // };
-    // var name_view = ["HOME", "PROJECT1","PROJECT2","PROJECT3","PROJECT4","Etc."];
-    // var name_hover = ["HOME", "공부하는 습관","MINT","밥추","PROJECT4","Etc."];
+interface NavManuProps {
+  isToggleOpen:boolean;
+}
+const Nav = () => {
+      const [isToggleOpen, setIsToggleOpen] = useState(false);
+
+        const handleToggleOpen = () => {
+            setIsToggleOpen(!isToggleOpen);
+        };
+
   return(
-    <><div css={back}>
+    <>
+     <StyledHeader>
           <img src="/Logo.png" alt="로고 이미지" css={img} />
           <div css={nav}>
-              {/* {
-        name_view.map((name)=>{
-            return(
-                <span
-                    css={isHovering? hover : ""}
-                    onMouseOver={handleMouseOver}
-                    onMouseOut={handleMouseOut}>
-                    <a css={link}> {name} </a>
-                </span>
-            )
-        })
-    }
-     {
-        name_hover.map((name)=>{
-            return(
-                <span
-                    css={isHovering? view : hover}
-                    onMouseOver={handleMouseOver}
-                    onMouseOut={handleMouseOut}>
-                    <a css={link}> {name} </a>
-                </span>
-            )
-        })
-    } */}
-              <span css={view}>
-                  <Link to='/' css={link}> HOME </Link>
-              </span>
-              
-              <span className="hide">
-                   <Link to='/proj/1' css={link}> PROJECT1 </Link>
-              </span>
-              <span className="show">
-                  <Link to='/proj/1' css={link}> 공부하는 습관 </Link>
-              </span>
-              <span className="hide">
-                <Link to='/proj/2' css={link}> PROJECT2 </Link>
-              </span>
-              <span className="show">
-                  <Link to='/proj/2' css={link}> MINT </Link>
-              </span>
-               <span className="hide">
-                  <Link to='/proj/3' css={link}> PROJECT3 </Link>
-              </span>
-              <span className="show">
-                  <Link to='/proj/3' css={link}> 밥추 </Link>
-              </span>
-              <span css={view}>
-                  <Link to='/proj/4' css={link}> PROJECT4 </Link>
-              </span>
-              <span css={view}>
-                  <Link to='/etc' css={link}> Etc. </Link>
-              </span>
-
-              
-          </div>
-      </div>
+              <NavManu isToggleOpen={isToggleOpen}>
+                <li>
+                    <span css={view}>
+                         <Link to='/' css={link}> HOME </Link>
+                    </span>
+                </li>
+                <li>
+                    <span className="hide">
+                        <Link to='/proj/1' css={link}> PROJECT1 </Link>
+                    </span>
+                    <span className="show">
+                        <Link to='/proj/1' css={link}> 공부하는 습관 </Link>
+                    </span>
+                </li>
+                <li>
+                    <span className="hide">
+                        <Link to='/proj/2' css={link}> PROJECT2 </Link>
+                    </span>
+                    <span className="show">
+                        <Link to='/proj/2' css={link}> MINT </Link>
+                    </span>
+                </li>
+                <li>
+                    <span className="hide">
+                        <Link to='/proj/3' css={link}> PROJECT3 </Link>
+                    </span>
+                    <span className="show">
+                        <Link to='/proj/3' css={link}> 밥추 </Link>
+                    </span>
+                </li>
+                <li>
+                    <span css={view}>
+                        <Link to='/proj/4' css={link}> PROJECT4 </Link>
+                    </span>
+                </li>
+                <li> 
+                    <span css={view}>
+                        <Link to='/etc' css={link}> Etc. </Link>
+                    </span>
+                </li>
+                </NavManu>
+                <FaBars className="menuToggleBtn" onClick={handleToggleOpen} />
+                </div>
+        </StyledHeader>
       <style>{`
         span.hide:hover{
            display: none;
@@ -92,15 +83,67 @@ const Nav = () => {
   );
   
 };
+const StyledHeader = styled.header`
+  font-family: "Pretendard";
+  color: #ffffff;
+  background-color: #9DC3E7;
+  box-shadow: 0px 5px 10px rgba(136, 136, 136, 0.25);
+  width: 100%;
+  // height:60px;
+  padding: 10px 12px 8px 12px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  .menuToggleBtn {
+    display: none;
+    color: white;
+    font-size: 24px;
+    position: absolute;
+    right: 15px;
+    top: 10px;
+    cursor: pointer;
+    align-items: center;
+
+  }
+
+  @media screen and (max-width: 768px) {
+    flex-direction: column;
+    align-items: flex-start;
+    
+    justify-content: center;
+    .menuToggleBtn {
+      display: flex;
+    }
+  }
+`;
+const NavManu = styled.ul<NavManuProps>`
+  list-style: none;
+  display: flex;
+
+  .nav-menu-list {
+    text-decoration: none;
+    color: white;
+    display: block;
+    padding: 10px 10px;
+    
+  }
+  @media screen and (max-width: 768px) {
+    display: ${(props) => (props.isToggleOpen ? "block" : "none")};
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+    // height: 50px;
+    margin-top: 20px;
+    li{
+      height: 50px;
+    }
+  }
+`;
 
 const view = css`
     &:hover {
         text-decoration : underline;
-    }
-`;
-const hover = css`
-    &:hover {
-        display: none;
     }
 `;
 const link = css`
@@ -110,26 +153,14 @@ const link = css`
     margin-left: 10px;
     margin-right: 10px;
     width: 150px;
-    height: 60px;
+    // height: 50px;
     align-items: center;
     display: flex;
     justify-content: center;
     cursor: pointer;
 `;
 
-const back = css`
-    display: flex;
-    top: 0;
-    left: 0;
-    align-items: center;
-    justify-content: space-between;
-    font-family: "Pretendard";
-    width: 100%;
-    height: 60px;
-    background-color: #9DC3E7;
-    box-shadow: 0px 5px 10px rgba(136, 136, 136, 0.25);
-    color: #ffffff;
-`;
+
 const nav = css`
     z-index: 5;
     // position: fixed;
